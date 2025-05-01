75a8b64ddedbe27f6612c50487ee2316bc62bc51
Mario <mariopereztoranzo1@gmail.com>
2025-05-02 01:02:53 +0200

    usuarios admin

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String },
   password: { type: String, required: true },
   points: { type: Number, default: 0 },
-  qrCode: { type: String, required: true, unique: true }
+  qrCode: { type: String, required: true, unique: true },
+  tipo: { type: String, required: true, default: "usuario" }
 }, { collection: "usuarios" });
 
 export default mongoose.model("User", userSchema);

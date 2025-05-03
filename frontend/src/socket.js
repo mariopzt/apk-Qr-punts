bf62842419553a645967ca84b2213472bf0dd866
Mario <mariopereztoranzo1@gmail.com>
2025-05-04 01:55:27 +0200

    err

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
 
-const URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:3001";
+const URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:5000"; // Cambiado a 5000 para coincidir con backend
+
 export const socket = io(URL, { autoConnect: false });
